ca615d64e3d696540aaddb998d5e1066b5de70c7
Scentor <neo25117832@gmail.com>
2026-03-12 20:45:28 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,5 @@
 const scriptData = {
   "name": "HomeWork.py",
   "desc": "Задача для домашнього опрацювання",
-  "content": "k = float(input("Введіть вагу Кенгуру (k): "))
-s = float(input("Введіть вагу сумки (s): "))
-p = float(input("Введіть вагу одного предмета (p): "))
-
-dog_weight = k - 30
-left_side = k + dog_weight
-needed_weight = left_side - s
-
-if needed_weight > 0:
-    count = needed_weight / p
-    print(count)
-else:
-    print(0)"
+  "content": "k = float(input(\"Введіть вагу Кенгуру (k): \"))\ns = float(input(\"Введіть вагу сумки (s): \"))\np = float(input(\"Введіть вагу одного предмета (p): \"))\n\ndog_weight = k - 30\nleft_side = k + dog_weight\nneeded_weight = left_side - s\n\nif needed_weight > 0:\n    count = needed_weight / p\n    print(count)\nelse:\n    print(0)"
 };
